8873611b1b6ee0646cd50503b1e384defe41e35b
Angel Miquelajauregui colli <angelmcolli06@outlook.com>
2025-11-21 13:10:40 -0600

    miquelingenieria version 2

---
+++ MIQUELINGENIERIA/Pages/Registro.cshtml.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MIQUELINGENIERIA.Models;
+using MySql.Data.MySqlClient;
+
+namespace MIQUELINGENIERIA.Pages
+{
+    public class RegistroModel : PageModel
+    {
+        [BindProperty]
+        public Usuario Usuario { get; set; }
+
+        public void OnGet() { }
+
+        public IActionResult OnPost()
+        {
+            string connectionString = "Server=localhost;Database=registrodb;Uid=root;Pwd=cisco123;";
+
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = @"INSERT INTO usuarios 
+                    (Nombres, Apellidos, FechaNacimiento, Correo, Telefono, Direccion) 
+                    VALUES (@Nombres, @Apellidos, @FechaNacimiento, @Correo, @Telefono, @Direccion)";
+
+                using (var cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Nombres", Usuario.Nombres);
+                    cmd.Parameters.AddWithValue("@Apellidos", Usuario.Apellidos);
+                    cmd.Parameters.AddWithValue("@FechaNacimiento", Usuario.FechaNacimiento);
+                    cmd.Parameters.AddWithValue("@Correo", Usuario.Correo);
+                    cmd.Parameters.AddWithValue("@Telefono", Usuario.Telefono);
+                    cmd.Parameters.AddWithValue("@Direccion", Usuario.Direccion);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            return RedirectToPage("Registro");
+        }
+    }
+}
---
+++ MIQUELINGENIERIA/Usuarios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MIQUELINGENIERIA.Models
+{
+    public class Usuario
+    {
+        [Required, StringLength(100)]
+        public string Nombres { get; set; }
+
+        [Required, StringLength(100)]
+        public string Apellidos { get; set; }
+
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required, EmailAddress, StringLength(100)]
+        public string Correo { get; set; }
+
+        [Required, StringLength(20)]
+        public string Telefono { get; set; }
+
+        [Required, StringLength(200)]
+        public string Direccion { get; set; }
+    }
+}
